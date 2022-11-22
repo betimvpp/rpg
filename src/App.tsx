43ec8bd910498d3380d468b5fb7eb97dc7584d79
betimvpp <betimvpp@gmail.com>
2022-11-22 09:35:26 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as C from './App.styles'
+import { Character } from './components/Character';
+import { useCharacter } from './hooks/useCharacter';
+import { useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App= () =>{
+  const char = useCharacter('Betim');
+
+  useEffect(()=>{
+    window.addEventListener('keydown', handleKeyDown);
+  }, []);
+
+  const handleKeyDown = (e: KeyboardEvent) =>{
+    switch(e.code) {
+      case 'KeyA':
+      case 'ArrowLeft':
+        char.moveLeft();
+      break;
+
+      case 'KeyD':
+      case 'ArrowRight':
+        char.moveRight();
+      break;
+
+      case 'KeyW':
+      case 'ArrowUp':
+        char.moveUp();
+      break;
+
+      case 'KeyS':
+      case 'ArrowDown':
+        char.moveDown();
+      break;
+    }
+  }
+
+  return(
+    <C.Container>
+      <C.Map>
+        <Character x={char.x} y={char.y} side={char.side}/>
+      </C.Map>
+    </C.Container>
+  )
 }
 
 export default App;
